8ceaab2622a811c4b9911024cf2171e352b708da
Polina Kaleturina <pikaleturina@edu.hse.ru>
2023-10-23 12:33:00 +0300

    modified test.py

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-print("hello world")
+def transpose_matrix(matrix):
+    # transpose the matrix
+    return [[row[i] for row in matrix] for i in range(len(matrix[0]))]
+transposed_matrix = transpose_matrix([[1,2,3],[4,5,6],[7,8,9]])
+for row in transposed_matrix:
+    print(row)
+
